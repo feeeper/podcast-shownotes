90b919b1523bb56bd57de1eacbee167705951fdd
Andrei Shirobokov <shirobokov.andrey@gmail.com>
2024-07-22 00:04:57 +0300

    couple simple tests for get_sentences

---
+++ tests/test_get_sentences.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+from src.components.segmentation.sentences import get_sentences
+
+
+def test_get_deepgram_sentences():
+    sentences = get_sentences(Path('transcription-deepgram.json'))
+    assert len(sentences) == 754
+
+
+def test_get_non_deepgram_sentences():
+    sentences = get_sentences(Path('episode-0122.mp3-large.json'))
+    assert len(sentences) == 1259
