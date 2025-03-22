ececd6d0eed25123794315c398cc147c4a7a66e5
Andrei Shirobokov <shirobokov.andrey@gmail.com>
2025-03-22 22:46:45 +0300

    fix tests

--- tests/test_get_sentences.py
+++ tests/test_get_sentences.py
@@ -5,10 +5,10 @@ from src.components.segmentation.sentences import get_sentences
 
 
 def test_get_deepgram_sentences():
-    sentences = get_sentences(Path('transcription-deepgram.json'))
+    sentences = get_sentences(Path('tests/transcription-deepgram.json'))
     assert len(sentences) == 754
 
 
 def test_get_non_deepgram_sentences():
-    sentences = get_sentences(Path('episode-0122.mp3-large.json'))
+    sentences = get_sentences(Path('tests/episode-0122.mp3-large.json'))
     assert len(sentences) == 1259
